964f75bbb42459a74d41e6601b893fb58da22eb9
Rishabh <rawatrishi1121@gmail.com>
2025-10-16 08:25:10 +0530

    completed the project

--- public/js/script.js
+++ public/js/script.js
@@ -10,8 +10,47 @@ if (navigator.geolocation) {
       console.error(err);
     },
     {
-        enableHighAccuracy:true,
-        maximumAge:0
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
     }
   );
 }
+
+const map = L.map("map").setView([0, 0], 10);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "OpenStreetMap",
+}).addTo(map);
+
+//
+
+const markers = {};
+
+const defaultIcon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = defaultIcon;
+
+socket.on("receive-location", (data) => {
+  console.log("Received:", data);
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude]);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
+
+socket.on("user-disconnected", (id) => {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
